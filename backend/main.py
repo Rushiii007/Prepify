6c9f94c760e0c7151ef4ff2edb642d1ebf6b9f25
Rushiii007 <2024mcb1301@iitrpr.ac.in>
2026-06-26 16:02:41 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ import traceback
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://prepify-blond.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
